eaf957c06ab8f63823d1f643f5d4dd7009732de6
KeerthanaaKumar <231901023@rajalakshmi.edu.in>
2026-06-28 17:00:40 +0530

    Add DTOs to prevent entity over-exposure in API responses

--- src/main/java/com/example/attendancesystem/dto/FailingMarkDTO.java
+++ src/main/java/com/example/attendancesystem/dto/FailingMarkDTO.java
@@ -1,4 +1,14 @@
 package com.example.attendancesystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class FailingMarkDTO {
+    private Long marksId;
+    private String studentName;
+    private String subjectName;
+    private String examType;
+    private Double score;
 }
